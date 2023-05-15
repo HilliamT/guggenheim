dabb66471dae34cf6c3a445ac776624e37ac70e8
Hilliam Tung <hilliamtung@gmail.com>
2023-05-15 16:08:00 +0100

    Added basic next + wagmi website

---
+++ website/pages/index.tsx
@@ -0,0 +1,36 @@
+import { createPublicClient, http } from 'viem';
+import { createConfig, mainnet, useAccount, useConnect, useDisconnect, WagmiConfig } from 'wagmi';
+import { InjectedConnector } from "wagmi/connectors/injected";
+
+const config = createConfig({
+    autoConnect: true,
+    publicClient: createPublicClient({
+        chain: mainnet,
+        transport: http()
+    }),
+});
+
+function Profile() {
+    const { address, isConnected } = useAccount()
+    const { connect } = useConnect({
+        connector: new InjectedConnector(),
+    })
+    const { disconnect } = useDisconnect()
+
+    if (isConnected)
+        return (
+            <div>
+                Connected to {address}
+                <button onClick={() => disconnect()}>Disconnect</button>
+            </div>
+        )
+    return <button onClick={() => connect()}>Connect Wallet</button>
+}
+
+export default function App() {
+    return (
+        <WagmiConfig config={config}>
+            <Profile />
+        </WagmiConfig>
+    )
+}
